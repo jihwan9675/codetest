360793e1c38ff9ff80946fb0d293621dbbccffa7
jihwan9675 <69146451+jihwan9675@users.noreply.github.com>
2021-06-08 22:28:09 +0900

    Create mergeSort.py

---
+++ algorithm/mergeSort.py
@@ -0,0 +1,9 @@
+def merge_sort(list):
+    if len(list) <= 1:
+        return list
+    mid = len(list) // 2
+    leftList = list[:mid]
+    rightList = list[mid:]
+    leftList = merge_sort(leftList)
+    rightList = merge_sort(rightList)
+    return merge(leftList, rightList)
