df6a48076197d67d4ae93b7f352a18d82bc0826f
jihwan9675 <69146451+jihwan9675@users.noreply.github.com>
2021-06-20 21:43:37 +0900

    Create dfs.py

---
+++ algorithm/dfs.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+def DFS(graph, root):
+    visited = []
+    stack = [root]
+
+    while stack:
+        n = stack.pop()
+        if n not in visited:
+            visited.append(n)
+            if n in graph:
+                temp = list(set(graph[n]) - set(visited))
+                temp.sort(reverse=True)
+                stack += temp
+    return " ".join(str(i) for i in visited)
