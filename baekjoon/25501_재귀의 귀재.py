fe563cb830dac7353970cbbd390c2f45f5489945
jihwan9675 <69146451+jihwan9675@users.noreply.github.com>
2023-07-28 00:32:14 +0900

    Create 25501_재귀의 귀재.py

---
+++ baekjoon/25501_재귀의 귀재.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+def recursion(s, l, r, cnt):
+    if l >= r: return 1, cnt
+    elif s[l] != s[r]: return 0, cnt
+    else: return recursion(s, l+1, r-1, cnt+1)
+
+def isPalindrome(s):
+    return recursion(s, 0, len(s)-1, 1)
+
+for i in range(N):
+    x, y = isPalindrome(input())
+    print(x, y)
