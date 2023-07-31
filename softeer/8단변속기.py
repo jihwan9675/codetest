56c539346b42c41f7f9b11d5cd02d05037d805d5
jihwan9675 <69146451+jihwan9675@users.noreply.github.com>
2023-08-01 01:51:34 +0900

    Create 8단변속기.py

---
+++ softeer/8단변속기.py
@@ -0,0 +1,11 @@
+import sys
+
+lsC = ''.join(list(input().split()))
+
+if lsC == '12345678':
+    print('ascending')
+elif lsC == '87654321':
+    print('descending')
+else:
+    print('mixed ')
+        
